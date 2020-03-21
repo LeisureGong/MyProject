861e4af679dbea99e40168cca66027950aaeebb7
gonglei <gonglei@encdata.cn>
2020-03-21 15:19:36 +0800

    添加配置信息

--- src/main/java/com/leisure/myproject/MyProjectApplication.java
+++ src/main/java/com/leisure/myproject/MyProjectApplication.java
@@ -2,10 +2,25 @@ package com.leisure.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(value = "com.leisure.myproject.dao",annotationClass = Repository.class)
 public class MyProjectApplication {
 
+	/**
+	* 统一设置时区
+	*/
+	@PostConstruct
+	void setDefaultTimezone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
--- src/main/java/com/leisure/myproject/controller/HelloController.java
+++ src/main/java/com/leisure/myproject/controller/HelloController.java
@@ -1,7 +1,20 @@
 package com.leisure.myproject.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "打个招呼")
+@RestController
+@RequestMapping("/api/hello")
 public class HelloController {
 
+	@ApiOperation(value = "你好呀")
+	@GetMapping("/addHello")
+	public void addHello(){
+		System.out.println("你好哈好啊好哦嗷嗷哈后!");
+	}
 }
